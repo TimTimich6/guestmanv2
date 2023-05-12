28b41ccefcea09906768e7a3c4de08853f48f977
TimTimich6 <obraztsov.timo@gmail.com>
2023-05-12 11:44:10 -0700

    loaded

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,25 @@ import { GuestI } from "@/lib/prisma/guest";
 export default function Home() {
   const [name, setName] = useState<string>("");
   const [guests, setGuests] = useState<GuestI[] | []>([]);
+  const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(false);
   async function upload(e: FormEvent) {
     e.preventDefault();
     e.stopPropagation();
     const resp = await axios.post("api/guest", { name }).catch((err) => {});
     if (resp) {
       getGuests();
+      setName("");
     }
   }
   async function getGuests() {
+    setLoading(true);
     const resp = await axios.get("api/guest").catch((err) => {});
     if (resp) {
       setGuests(resp.data.guests);
+      setCount(resp.data.guests.length);
     }
+    setLoading(false);
   }
 
   async function deleteGuest(name1: string) {
@@ -49,7 +55,9 @@ export default function Home() {
           </h1>
         </button>
       </form>
-      <div className="flex flex-1 flex-col bg-gray-800 w-full items-center p-5 gap-3">
+      {loading && <div className="w-[7rem] h-[7rem] rounded-[50%] border-t-indigo-500 border-[1.5rem] animate-spin"></div>}
+      <div className="flex flex-1 flex-col w-max items-center p-5 gap-3">
+        <h1 className="text-white m-3 font-bold text-2xl self-start">Total: {count}</h1>
         {guests &&
           guests.map((guest: GuestI) => {
             return <Guest name={guest.name} time={new Date(guest.createdAt!)} onClick={deleteGuest}></Guest>;
